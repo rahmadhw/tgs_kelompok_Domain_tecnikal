14bac80dd1530f9b0ea3f8810504dd1f1c043095
rahmadhw <rahmadhidyat098@gmail.com>
2022-04-12 22:04:00 +0700

    Update Selesai

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, jsonify
+import joblib
+import numpy as np
 
 
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 
+def predik():
+    if request.method == "GET":
+        return render_template("index.html")
+    elif request.method == "POST":
+        pred = [int(x) for x in request.form.values()]
+        final_pred =[np.array(pred)]
+        model,std_scaler = joblib.load("Modelling/model_joblib/model_data_fix.pkl")
+        result = model.predict(final_pred)
+        print(result)
+        output = str(round(result[0], 2))
+        return render_template("index.html", result = output)
+    else:
+        return "Error"
 
-def index():
-    return render_template("index.html")
 
 
 if __name__ == '__main__':
